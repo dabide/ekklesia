1ad85e671925c19778841a0f0fd03c166e5acc70
David A. Sjøen <david@pilgrim.no>
2021-04-11 21:15:49 +0200

    Made ServiceStack update script both check for latest version and build

--- scripts/BuildServiceStackLibraries.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-
-Push-Location "$PSScriptRoot/../api"
-docker build --target=ss-lib -o type=local,dest="$PSScriptRoot/../api" ./Ekklesia.Api
-Pop-Location
--- scripts/UpdateServiceStack.ps1
+++ scripts/UpdateServiceStack.ps1
@@ -10,8 +10,13 @@ if ([string]::IsNullOrEmpty($serviceStackVersion)) {
     throw
 }
 
-(Get-Content "$PSScriptRoot/../api/Ekklesia.Api/Dockerfile") `
+Push-Location "$PSScriptRoot/../api"
+
+(Get-Content "./Ekklesia.Api/Dockerfile") `
     -replace '^ARG SS_VERSION=.*$', "ARG SS_VERSION=$serviceStackVersion" |
-        Out-File "$PSScriptRoot/../api/Ekklesia.Api/Dockerfile"
+        Out-File "./Ekklesia.Api/Dockerfile"
 
 Write-Host "The latest ServiceStack version is $serviceStackVersion"
+
+docker build --target=ss-lib -o type=local,dest="$PSScriptRoot/../api" ./Ekklesia.Api
+Pop-Location
